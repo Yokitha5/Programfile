dcb5b577dc933bb01b65c8efd97f83b762b892d4
Yokitha Ramalingam <yogitha21363@gmail.com>
2026-04-24 21:41:42 +0530

    Implement divide and isPalindrome methods

---
+++ practice/Day7.java
@@ -0,0 +1,62 @@
+// 1.Divide Two Integers
+class Solution {
+    public int divide(int dividend, int divisor) {
+        
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        long dvd = Math.abs((long) dividend);
+        long dvs = Math.abs((long) divisor);
+
+        int result = 0;
+
+        while (dvd >= dvs) {
+            long temp = dvs;
+            int multiple = 1;
+
+            while (dvd >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+            result += multiple;
+        }
+
+        if ((dividend < 0) ^ (divisor < 0)) {
+            result = -result;
+        }
+
+        return result;
+    }
+}
+
+//  2. Valid Palindrome
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != 
+                Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
